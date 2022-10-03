9e8fe46b591a64071c2e492a8d381402bd8d3b71
ralf.brauchler <ralf.brauchler@codecentric.de>
2022-10-03 11:54:07 +0200

    Add axis to visualisation

--- visualgas.py
+++ visualgas.py
@@ -59,11 +59,31 @@ for entry in data:
                 dailyMoments.append(moment.replace(second=0, microsecond=0, minute=00, hour=12))
 
     if entry["text"] == "sum of gas":
-        sum.append((float(entry["value"])-18620)/10)
+        sum.append(float(entry["value"]))
 
-
-plt.bar(hourlyMoments, hourly, width=0.036, color = "#aa0000", alpha = 0.6)
-plt.bar(dailyMoments, daily, width=1, color = "#ccccff", alpha = 0.6)
-plt.plot(moments, sum, color = "#00aa00", alpha = 0.6)
 # plt.scatter(moments, usage, color = "#0000aa", alpha = 0.6)
+# plt.show()
+
+fig, ax1 = plt.subplots()
+
+hourlyColor = "#0000aa"
+dailyColor = "#ccccff"
+m3Color = "#00aa00"
+
+ax1.set_xlabel('time')
+ax1.set_ylabel('kWh / Tag', color=dailyColor)
+ax1.bar(dailyMoments, daily, color = dailyColor, alpha = 0.6)
+ax1.tick_params(axis='y', labelcolor=dailyColor)
+
+ax2 = ax1.twinx()
+ax2.set_ylabel('kWh / Stunde', color=hourlyColor)
+ax2.bar(hourlyMoments, hourly, width=0.036, color=hourlyColor, alpha = 0.6)
+ax2.tick_params(axis='y', labelcolor=hourlyColor)
+
+ax3 = ax1.twinx()
+ax3.set_ylabel('m3', color=m3Color, labelpad=-10)
+ax3.tick_params(axis='y', pad=30, labelcolor=m3Color) # labelleft = True
+ax3.plot(moments, sum, color = m3Color, alpha = 0.6)
+
+fig.tight_layout()
 plt.show()
